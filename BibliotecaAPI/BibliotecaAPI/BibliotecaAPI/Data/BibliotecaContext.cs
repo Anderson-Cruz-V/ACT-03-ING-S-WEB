97242720d135fb2783b0fb5f916ba48cc2773768
Anderson-Cruz-V <213361341+Anderson-Cruz-V@users.noreply.github.com>
2026-06-25 01:11:59 -0400

    corregir

--- BibliotecaAPI/BibliotecaAPI/BibliotecaAPI/Data/BibliotecaContext.cs
+++ BibliotecaAPI/BibliotecaAPI/BibliotecaAPI/Data/BibliotecaContext.cs
@@ -1,15 +1,1 @@
-﻿using BibliotecaAPI.Models;
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-
-namespace BibliotecaAPI.Data
-{
-    public class BibliotecaContext : DbContext
-    {
-        public BibliotecaContext(DbContextOptions<BibliotecaContext> options) : base(options)
-        {
-        }
-
-        public DbSet<Libro> Libros { get; set; }
-    }
-}
+﻿
